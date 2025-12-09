7fe3799ee95ac0fbd91631816ffb40f11ebdc9be
ahlem002 <bounasriahlem@gmail.com>
2025-12-09 11:21:00 +0100

    fake data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (id, name, location, phone, head) VALUES
+INSERT INTO department (idDepartment,name, location, phone, head) VALUES
 (1, 'Computer Science', 'Building A, Floor 2', '+216 71 123 456', 'Dr. Ahmed Ben Salah'),
 (2, 'Mathematics', 'Building B, Floor 1', '+216 71 123 457', 'Prof. Leila Trabelsi'),
 (3, 'Physics', 'Building C, Floor 3', '+216 71 123 458', 'Dr. Karim Gharbi'),
